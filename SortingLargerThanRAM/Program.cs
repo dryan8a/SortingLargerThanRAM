72dd73fb3af45c6d722494ed81266f3906ae1eee
dryan8a@gmail.com <dryan8a@gmail.com>
2021-02-24 13:10:54 -0800

    Fixed some bugs regarding resetting the filestream position and with they .copy function. I am now splitting the dataset into multiple files based on my theoretical amount of RAM. I then sort each of these files. Next step is to look into k-way merge and combine the files back into the original

--- SortingLargerThanRAM/Program.cs
+++ SortingLargerThanRAM/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using System.IO;
+using System.Collections.Generic;
 
 namespace SortingLargerThanRAM
 {
@@ -9,20 +10,32 @@ namespace SortingLargerThanRAM
         static void Main(string[] args)
         {
             const int NumberOfCPUCores = 4; //find using task manager
-            const int AmountOfRam = 20000000; //not necessarily accurate amount of RAM dummed down so that I can actually see the sort going on multiple cores
+            const int AmountOfRam = 5000000; //not necessarily accurate amount of RAM dummed down so that I can actually see the sort going on multiple cores
 
             var stream = File.OpenRead("city_temperature.csv");
-            long length = stream.Length;
-            int amountOfNeededFiles = (int)(length / AmountOfRam) + 1;
+            int amountOfNeededFiles = (int)(stream.Length / AmountOfRam) + 1;
 
+            var sortingStreams = new List<FileStream>();
+            for(int i = 0;i<amountOfNeededFiles;i++)
+            {
+                sortingStreams.Add(File.Create($"file{i}.txt"));
+
+                var bytesToCopy = new byte[AmountOfRam];
+                stream.Read(bytesToCopy, 0, AmountOfRam);
+
+                int bytesCount = (int)(i == amountOfNeededFiles - 1 ? stream.Length % AmountOfRam : AmountOfRam); //ensures that the overlap in the last file isn't filled with whitespace
+                sortingStreams[i].Write(bytesToCopy,0,bytesCount);
+                sortingStreams[i].Position = 0; //resets position of stream for read
+            }
+            
             Parallel.For(0, amountOfNeededFiles, i =>
             {
-                var currentFileStream = File.Create($"file{i}.txt");
-                stream.CopyTo(currentFileStream, AmountOfRam);
-                ;
+                var bytes = new byte[AmountOfRam];
+                sortingStreams[i].Read(bytes, 0, AmountOfRam);
+                Sorts.MergeSort(bytes);
+                sortingStreams[i].Position = 0; //resets position of stream for write so that the file size doesn't double
+                sortingStreams[i].Write(bytes, 0, AmountOfRam);
             });
         }
-
-       
     }
 }
